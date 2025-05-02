d556769a2fd69c4354a58647adc4b2f1a2634ede
Sowdarjya <kolaysowdarjya@gmail.com>
2025-05-02 15:51:02 +0530

    feat: initialize client application with Vite and React
    - Added package.json for project dependencies and scripts.
    - Included Vite logo SVG in public directory.
    - Created App.css for styling the application.
    - Implemented main App component with a Recorder component.
    - Added React logo SVG in assets directory.
    - Developed Recorder component for audio recording functionality.
    - Created index.css for global styles.
    - Set up main entry point (main.jsx) for rendering the application.
    - Configured Vite with a basic setup in vite.config.js.

---
+++ client/src/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Recorder from "./components/Recorder";
+
+const App = () => {
+  return (
+    <div>
+      App
+      <Recorder />
+    </div>
+  );
+};
+
+export default App;
