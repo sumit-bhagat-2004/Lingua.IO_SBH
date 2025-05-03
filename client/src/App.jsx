f9ea305ec24eb949c695ead3d8918cfff37edd6d
sumit-bhagat-2004 <sumitbhagat011@gmail.com>
2025-05-03 11:03:42 +0530

    Pronounciation Checker merged

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,13 +1,10 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import HomePage from "./pages/HomePage";
 import { Routes, Route } from "react-router-dom";
 import OnboardingGate from "./components/OnboardingGate";
 import Dashboard from "./pages/Dashboard";
 import OnboardingPage from "./pages/OnboardingPage";
-import Recorder from "./components/Recorder";
-import PronunciationChecker from "./components/Pronounciationchecker";
 
 const App = () => {
   return (
@@ -24,16 +21,9 @@ const App = () => {
           }
         />
         <Route path="/onboarding" element={<OnboardingPage />} />
-      </Routes>
-    </div>
-    <Router>
-      <Navbar />
-      <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/grammar" element={<Recorder />} />
         <Route path="/pronounciation" element={<PronunciationChecker />} />
       </Routes>
-    </Router>
+    </div>
   );
 };
 
